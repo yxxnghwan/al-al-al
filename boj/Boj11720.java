f1175489211daefe672157e9457f95c45f5b8a00
yxxnghwan <younghwan960@gmail.com>
2023-11-14 01:02:19 +0900

    solve boj11720

--- boj/Boj11720.java
+++
@@ -1,7 +0,0 @@
-package boj;
-
-public class Boj11720 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-    }
-}
---
+++ boj/Boj11720_B4.kt
@@ -0,0 +1,14 @@
+package boj
+
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine()
+
+    val numbers = br.readLine()
+        .toCharArray()
+        .map { it - '0' }
+    println(numbers.sum())
+}
